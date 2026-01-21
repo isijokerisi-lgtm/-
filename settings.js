1ee03d74d47ecff8fd5daa24379ab278e76b250f
isijokerisi-lgtm <isijokerisi@gmail.com>
2026-01-21 15:37:32 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩  ʀᴇʜᴀɴ ᴍᴅ  𓆪',
+  author: '‎',
+  botName: "𓆩  ʀᴇʜᴀɴ-ᴍᴅ  𓆪",
+  botOwner: 'REHAN', // Your name
+  ownerNumber: '923259487091', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/isijokerisi-lgtm/-",
+};
+
+module.exports = settings;
